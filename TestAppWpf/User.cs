97372b551d8656afd8d8aa5055ab6f5343a539f4
Artyom <yosso117@gmail.com>
2019-12-26 08:53:19 +0300

    Add project files.

---
+++ TestAppWpf/User.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppWpf
+{
+    class User : INotifyPropertyChanged
+    {
+        private int index;
+        private string userName;
+        private string organization;
+        private string ip;
+        private string sessionId;
+        private DateTime loginTime;
+        private DateTime logoutTime;
+        private byte endCode;
+
+        public int Index { get 
+            { 
+                return index; 
+            } 
+            set 
+            {
+                index = value;
+                OnPropertyChanged("NumOrder"); 
+            } 
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        public string Organization
+        {
+            get
+            {
+                return organization;
+            }
+            set
+            {
+                organization = value;
+                OnPropertyChanged("Organization");
+            }
+        }
+
+        public string Ip
+        {
+            get
+            {
+                return ip;
+            }
+            set
+            {
+                ip = value;
+                OnPropertyChanged("Ip");
+            }
+        }
+
+        public string SessionId
+        {
+            get
+            {
+                return sessionId;
+            }
+            set
+            {
+                sessionId = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public DateTime LoginTime
+        {
+            get
+            {
+                return loginTime;
+            }
+            set
+            {
+                loginTime = value;
+                OnPropertyChanged("LoginTime");
+            }
+        }
+
+        public DateTime LogoutTime
+        {
+            get
+            {
+                return logoutTime;
+            }
+            set
+            {
+                logoutTime = value;
+                OnPropertyChanged("LogoutTime");
+            }
+        }
+
+        public byte EndCode 
+        { 
+            get
+            {
+                return endCode;
+            }
+
+            set
+            {
+                endCode = value;
+                OnPropertyChanged("EndCode");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ TestAppWpf/UserViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestAppWpf.ViewModel
+{
+    class UserViewModel
+    {
+        private string path = @"..\..\TXT\LOG.txt";
+        private string errorPath = @"..\..\TXT\ERROR.txt";
+        private string ipPattern = @"^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?)$";
+        private string dateTimePattern = @"^([1-9]|([012][0-9])|(3[01])).([0]{0,1}[1-9]|1[012]).\d\d\d\d (20|21|22|23|[0-1]?\d):[0-5]?\d:[0-5]?\d$";
+        public ObservableCollection<User> Users { get; set; }
+
+        public UserViewModel()
+        {
+            Users = new ObservableCollection<User>();
+            File.WriteAllText(errorPath, string.Empty);
+            ParseLOG();
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private bool RegexCheck(User user)
+        {
+            return (Regex.IsMatch(user.Ip, ipPattern) && Regex.IsMatch(user.LoginTime.ToString(),dateTimePattern) && Regex.IsMatch(user.LogoutTime.ToString(),dateTimePattern));
+        }
+
+        private async void ParseLOG()
+        {
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = await sr.ReadLineAsync()) != null)
+                {
+                    string[] values = line.Split(';');
+
+                    try
+                    {
+                        User user = new User()
+                        {
+                            Index = Convert.ToInt32(values[0]),
+                            UserName = values[1],
+                            Organization = values[2],
+                            Ip = values[3],
+                            SessionId = values[4],
+                            LoginTime = Convert.ToDateTime(values[5]),
+                            LogoutTime = Convert.ToDateTime(values[6])
+                        };
+
+                        if (RegexCheck(user))
+                        {
+                            Users.Add(user);
+                        }
+                        else
+                        {
+                            WriteERROR(string.Format("{0} [IP adress and/or DateTime value is incorrect]", line));
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        WriteERROR(string.Format("{0} [ERROR] : [{1}]", line, ex.Message));
+                        continue;
+                    }
+
+
+                }
+            }
+        }
+
+        private async void WriteERROR(string line)
+        {
+            using (StreamWriter sw = new StreamWriter(errorPath, true, Encoding.Default))
+            {
+                await sw.WriteLineAsync(line);
+            }
+        }
+
+    }
+    
+}
